4962109fe443e0369ab9bf71409d5d3d62805293
Laura <Laura@DESKTOP-BESSB6E>
2023-09-11 22:00:34 +0300

    Add project files.

---
+++ 1.1Uzduotis_Ref_Out/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _1._1Uzduotis_Ref_Out
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 2;
+           
+            IncrementByN(ref x, ref y);
+            Console.WriteLine(x);
+        }
+        public static void IncrementByN(ref int x, ref int y)
+
+        {
+            x += y;   
+        }
+    }
+}
